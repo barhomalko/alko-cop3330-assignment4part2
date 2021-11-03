3ab4a6911834a7222e93d7dba540aef79fdfcd4c
barhom <barhom@knights.ucf.edu>
2021-11-02 22:31:42 -0400

    check

---
+++ src/main/java/ucf/assignments/App.java
@@ -0,0 +1,37 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 barhom alko
+ */
+
+
+
+package ucf.assignments;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+/*starts up the app */
+
+
+public class App {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("App.fxml")));
+        stage.setTitle("Application Assignment");
+        stage.setScene(new Scene(root));
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
